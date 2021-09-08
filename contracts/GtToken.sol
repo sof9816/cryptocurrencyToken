f6bc8df929fb3d2052eb23e54320be10cc07732a
Mustafa Naser <sof9816@gmail.com>
2021-09-08 23:23:04 +0300

    :white_check_mark: Done transferFrom, approve, and allowance with some test.

--- contracts/GtToken.sol
+++ contracts/GtToken.sol
@@ -2,30 +2,61 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract GtToken {
+  string public name = "Gt Token"; // this is erc-20 but optional
+  string public symbol = "GT"; // this is erc-20 but optional
+  string public standard = "GT Token v1.0"; // this is not erc-20, just a personal preferance
+  uint256 public totalSupply;
 
-    string public name = 'Gt Token'; // this is erc-20 but optional
-    string public symbol = 'GT'; // this is erc-20 but optional
-    string public standard = 'GT Token v1.0'; // this is not erc-20, just a personal preferance
-    uint256 public totalSupply;
-    
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
+  // Transfer event, erc-20 std
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  // Approval event, erc-20 std
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
-    mapping(address => uint256) public balanceOf;
-    
-    constructor() public {
-        balanceOf[msg.sender] = 1000000;
-        totalSupply = 1000000;
-        // allocate the initial supply
-    }
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
+  constructor() public {
+    balanceOf[msg.sender] = 1000000;
+    totalSupply = 1000000;
+    // allocate the initial supply
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value)
+    public
+    returns (bool success)
+  {
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _value
+  ) public returns (bool success) {
+    // require from account has enough tokens
+    require(_value <= balanceOf[_from], "value is bigger than the balance of sender");
+    // require allowance is big enough
+    require(_value <= allowance[_from][msg.sender],  "value is bigger than the allowance");
+    // change balance
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    // update allowance
+    allowance[_from][msg.sender] -= _value;
+    // Transfer event
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
 }
--- test/GtToken.js
+++ test/GtToken.js
@@ -89,4 +89,147 @@ contract("GtToken", (accounts) => {
         );
       });
   });
+  it("approves tokens for delegated transfer", () => {
+    return GtToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 100);
+      })
+      .then((success) => {
+        assert.equal(success, true, "return true");
+        return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+      })
+      .then((receipt) => {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          'should be the "Approval" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._owner,
+          accounts[0],
+          "logs the account the tokens are transferred from"
+        );
+        assert.equal(
+          receipt.logs[0].args._spender,
+          accounts[1],
+          "logs the account the tokens are transferred to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          100,
+          "logs the transfer amount"
+        );
+        return tokenInstance.allowance(accounts[0], accounts[1]);
+      })
+      .then((allowance) => {
+        assert.equal(
+          allowance.toNumber(),
+          100,
+          "stores the allowance for delegated transfer"
+        );
+      });
+  });
+
+  it("handles delegated token transfers", () => {
+    return GtToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+        return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+      })
+      .then((receipt) => {
+        return tokenInstance.approve(spendingAccount, 10, {
+          from: fromAccount,
+        });
+      })
+      .then((receipt) => {
+        return tokenInstance.transferFrom(
+          fromAccount,
+          toAccount,
+          999999999999999999999999999999,
+          {
+            from: spendingAccount,
+          }
+        );
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert.equal(
+          error.message.toString().includes("fault"),
+          false,
+          "cannot transfer value larger than balance"
+        );
+        return tokenInstance.transferFrom(fromAccount, toAccount, 20, {
+          from: spendingAccount,
+        });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert.equal(
+          error.message.toString().includes("fault"),
+          false,
+          "cannot transfer value larger than approved amount"
+        );
+        return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {
+          from: spendingAccount,
+        });
+      })
+      .then((success) => {
+        assert.equal(success, true);
+        return tokenInstance.transferFrom(fromAccount, toAccount, 10, {
+          from: spendingAccount,
+        });
+      })
+      .then((receipt) => {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Transfer",
+          'should be the "Transfer" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._from,
+          fromAccount,
+          "logs the account the tokens are transferred from"
+        );
+        assert.equal(
+          receipt.logs[0].args._to,
+          toAccount,
+          "logs the account the tokens are transferred to"
+        );
+        assert.equal(
+          receipt.logs[0].args._value,
+          10,
+          "logs the transfer amount"
+        );
+        return tokenInstance.balanceOf(fromAccount);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          90,
+          "deducts the amount from the sending account"
+        );
+        return tokenInstance.balanceOf(toAccount);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          10,
+          "adds the amount to the receving account"
+        );
+        return tokenInstance.allowance(fromAccount, spendingAccount);
+      })
+      .then((allowance) => {
+        assert.equal(
+          allowance.toNumber(),
+          0,
+          "deducts the amount from the allowance"
+        );
+      });
+  });
 });
